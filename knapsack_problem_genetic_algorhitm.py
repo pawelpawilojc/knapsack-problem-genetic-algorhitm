6462bd542fe5ce774f37ae56900b368959c2f2ce
Paweł Pawiłojć <pawilojc@gmail.com>
2025-01-24 23:38:44 +0100

    Add brute force solution and generating new populations

--- knapsack_problem_genetic_algorhitm.py
+++ knapsack_problem_genetic_algorhitm.py
@@ -80,11 +80,14 @@ def print_chromosome(chromosome: list[int], weights: list[int], values: list[int
 weights_list, values_list, capacities_list = read_data()
 
 
-row_number = 200
-print_row(row_number)
-weights = weights_list[row_number]
-values = values_list[row_number]
-capacity = capacities_list[row_number]
+def read_data_from_row(row_number: int):
+    weights = weights_list[row_number]
+    values = values_list[row_number]
+    capacity = capacities_list[row_number]
+    return weights, values, capacity
+
+
+weights, values, capacity = read_data_from_row(200)
 population = generate_starting_population(10)
 
 
@@ -132,18 +135,28 @@ print_chromosome(test_2, weights, values)
 # 2. Opracować algorytm selekcji turniejowej:
 # a. Zaimplementować proces losowego wyboru osobników do turniejów.
 # b. Zaprojektować mechanizm wyboru najlepszego osobnika z każdego turnieju.
+
 def tournament_selection(population, weights, values, capacity):
     """Tournament selection returning winners - half of the population"""
+
     tournament_winners = []
-    while (len(population) > 0):
-        chromosome_1 = random.choice(population)
-        population.remove(chromosome_1)
-        chromosome_2 = random.choice(population)
-        population.remove(chromosome_2)
+    population_copy = population.copy()
+
+    while len(population_copy) >= 2:
+
+        chromosome_1 = random.choice(population_copy)
+        population_copy.remove(chromosome_1)
+        chromosome_2 = random.choice(population_copy)
+        population_copy.remove(chromosome_2)
+
         if fitness(chromosome_1, weights, values, capacity) > fitness(chromosome_2, weights, values, capacity):
             tournament_winners.append(chromosome_1)
         else:
             tournament_winners.append(chromosome_2)
+
+    if len(population_copy) == 1:
+        tournament_winners.append(population_copy[0])
+
     return tournament_winners
 
 # 3. Opracować algorytm selekcji proporcjonalnej (metoda ruletki):
@@ -158,7 +171,7 @@ def roulette_selection(population, weights, values, capacity):
     roulette_population = []
     for _, chromosome in enumerate(population):
         counter = 0
-        while counter < fitness(chromosome, weights, values, capacity):
+        while counter <= fitness(chromosome, weights, values, capacity):
             roulette_population.append(chromosome)
             counter += 1
     return random.choice(roulette_population)
@@ -188,3 +201,143 @@ def mutation(population, percent: int):
 print(population)
 population = mutation(population, 5)
 print(population)
+
+
+def chromosome_weight(chromosome, weights):
+    total_weight = 0
+
+    for i, gene in enumerate(chromosome):
+        if gene == 1:
+            total_weight += weights[i]
+
+    return total_weight
+
+
+def chromosome_value(chromosome, values):
+    total_value = 0
+
+    for i, gene in enumerate(chromosome):
+        if gene == 1:
+            total_value += values[i]
+
+    return total_value
+
+
+def solution(weights: list[int], values: list[int], capacity: int):
+    chromosome_solution = []
+    all_possible_chromosomes = []
+    all_possible_chromosomes.append([0, 0, 0, 0, 0])
+    all_possible_chromosomes.append([0, 0, 0, 0, 1])
+    all_possible_chromosomes.append([0, 0, 0, 1, 0])
+    all_possible_chromosomes.append([0, 0, 0, 1, 1])
+    all_possible_chromosomes.append([0, 0, 1, 0, 0])
+    all_possible_chromosomes.append([0, 0, 1, 0, 1])
+    all_possible_chromosomes.append([0, 0, 1, 1, 0])
+    all_possible_chromosomes.append([0, 0, 1, 1, 1])
+    all_possible_chromosomes.append([0, 1, 0, 0, 0])
+    all_possible_chromosomes.append([0, 1, 0, 0, 1])
+    all_possible_chromosomes.append([0, 1, 0, 1, 0])
+    all_possible_chromosomes.append([0, 1, 0, 1, 1])
+    all_possible_chromosomes.append([0, 1, 1, 0, 0])
+    all_possible_chromosomes.append([0, 1, 1, 0, 1])
+    all_possible_chromosomes.append([0, 1, 1, 1, 0])
+    all_possible_chromosomes.append([0, 1, 1, 1, 1])
+    all_possible_chromosomes.append([1, 0, 0, 0, 0])
+    all_possible_chromosomes.append([1, 0, 0, 0, 1])
+    all_possible_chromosomes.append([1, 0, 0, 1, 0])
+    all_possible_chromosomes.append([1, 0, 0, 1, 1])
+    all_possible_chromosomes.append([1, 0, 1, 0, 0])
+    all_possible_chromosomes.append([1, 0, 1, 0, 1])
+    all_possible_chromosomes.append([1, 0, 1, 1, 0])
+    all_possible_chromosomes.append([1, 0, 1, 1, 1])
+    all_possible_chromosomes.append([1, 1, 0, 0, 0])
+    all_possible_chromosomes.append([1, 1, 0, 0, 1])
+    all_possible_chromosomes.append([1, 1, 0, 1, 0])
+    all_possible_chromosomes.append([1, 1, 0, 1, 1])
+    all_possible_chromosomes.append([1, 1, 1, 0, 0])
+    all_possible_chromosomes.append([1, 1, 1, 0, 1])
+    all_possible_chromosomes.append([1, 1, 1, 1, 0])
+    all_possible_chromosomes.append([1, 1, 1, 1, 1])
+    chromosome_solution = all_possible_chromosomes[0]
+
+    chromosome_solution_value = chromosome_value(chromosome_solution, values)
+    for _, chromosome in enumerate(all_possible_chromosomes):
+        if chromosome_value(chromosome, values) > chromosome_solution_value and chromosome_weight(chromosome, weights) <= capacity:
+            chromosome_solution = chromosome
+            chromosome_solution_value = chromosome_value(chromosome, values)
+            print_chromosome(chromosome, weights, values)
+    return chromosome_solution
+
+
+def genetic_algorithm(population: list[list[int]], weights: list[int], values: list[int], capacity: int, selection: int, crossover: int, mutation_percent: int, generation_limit: int):
+    """Genetic algorithm for knapsack problem, main function. Returns solution - chromosome (list[int])
+    Arguments: population, weights, values, capacity,
+        selection:
+            1 - tournament selection,
+            2 - roulette selection;
+        crossover:
+            1 - 1 point crossover,
+            2 - 2 point crossover;
+        mutation_percent - int(1-100),
+        generation_limit - number of generations before stop.
+    """
+    print("\n\ngenetic algorithm")
+    print("population size:", len(population))
+    print("population: \n", population)
+    print("weights:", weights)
+    print("values:", values)
+    print("capacity:", capacity)
+    print("solution:", solution(weights, values, capacity))
+    print("selection:", selection)
+    print("crossover:", crossover)
+    print("mutation percent:", mutation_percent)
+
+    generation_counter = 0
+    while generation_counter < generation_limit:
+        new_population = []
+
+        while len(new_population) < len(population):
+
+            if selection == 1:
+                winners = tournament_selection(
+                    population, weights, values, capacity)
+                father = random.choice(winners)
+                mother = random.choice(winners)
+                counter = 0
+                while father == mother and counter < 3:
+                    mother = random.choice(winners)
+                    counter += 1
+            elif selection == 2:
+                father = roulette_selection(
+                    population, weights, values, capacity)
+                mother = roulette_selection(
+                    population, weights, values, capacity)
+                counter = 0
+                while father == mother and counter < 3:
+                    mother = roulette_selection(
+                        population, weights, values, capacity)
+                    counter += 1
+            if crossover == 1:
+                offspring_1, offspring_2 = crossover_1(father, mother, 3)
+                new_population.append(offspring_1)
+                new_population.append(offspring_2)
+            elif crossover == 2:
+                offspring_1, offspring_2 = crossover_2(father, mother, 2, 4)
+                new_population.append(offspring_1)
+                new_population.append(offspring_2)
+
+        generation_counter += 1
+        population = new_population
+        population = mutation(population, mutation_percent)
+        print(population)
+    return 0
+
+
+population = []
+population = generate_starting_population(10)
+weights, values, capacity = read_data_from_row(201)
+
+print("solution:")
+solution(weights, values, capacity)
+
+genetic_algorithm(population, weights, values, capacity, 2, 2, 2, 10)
